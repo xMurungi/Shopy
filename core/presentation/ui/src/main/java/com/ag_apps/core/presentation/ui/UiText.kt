b124a7876587ae0e400173b52b82a202ef5000f8
ahmed-guedmioui <ahmedgire01@gmail.com>
2024-09-04 18:45:27 +0100

    created string resource management

---
+++ core/presentation/ui/src/main/java/com/ag_apps/core/presentation/ui/UiText.kt
@@ -0,0 +1,38 @@
+package com.ag_apps.core.presentation.ui
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+
+/**
+ * @author Ahmed Guedmioui
+ */
+sealed interface UiText {
+
+    data class DynamicString(
+        val value: String
+    ) : UiText
+
+    class StringResource(
+        @StringRes val id: Int,
+        vararg val args: Array<Any> = arrayOf()
+    ) : UiText
+
+    @Composable
+    fun asString(): String {
+        return when(this) {
+            is DynamicString -> value
+            is StringResource -> stringResource(id, *args)
+        }
+    }
+
+    fun asString(context: Context): String {
+        return when(this) {
+            is DynamicString -> value
+            is StringResource -> context.getString(id, *args)
+        }
+    }
+}
